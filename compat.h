3734fdc8106c60b272a50ee359c11deab1550149
Damien Miller <djm@mindrot.org>
2007-07-05 12:01:59 +1000

    - (djm) Copyright year in all notices

--- compat.h
+++ compat.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Damien Miller <djm@mindrot.org>
+ * Copyright (c) 2007 Damien Miller <djm@mindrot.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
