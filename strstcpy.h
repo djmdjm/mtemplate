8aa08d5d81439020b3d99730df0d21d88b7ec51e
Damien Miller <djm@mindrot.org>
2015-12-16 11:57:02 +1100

    fix build; missing files

---
+++ strstcpy.h
@@ -0,0 +1,34 @@
+/*
+ * Based on strlcpy
+ *
+ * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
+ * Copyright (c) 2007 Damien Miller <djm@mindrot.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/* $Id: strstcpy.h,v 1.1 2007/04/02 08:55:06 djm Exp $ */
+
+#ifndef _STRSTCPY_H
+#include <sys/types.h>
+
+/*
+ * Copy up to "len" chars from "src" to "dst", stopping early if a '\0'
+ * or a character from "stopchars" is encountered. Returns the number
+ * of characters that would have been copied if "dst" was unlimited in
+ * length (a la strlcpy)
+ */
+size_t
+strstcpy(char *dst, const char *src, size_t len, const char *stopchars);
+
+#endif /* _STRSTCPY_H */
